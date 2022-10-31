4d33a01e494d36b34e3f81deb77aa338e9d3aac9
Grigory Tsiperman <113826725+GrigoryTsip@users.noreply.github.com>
2022-10-31 10:57:16 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       if (createDir("C://Games", "null")) {
+            if (createDir("C://Games/src", "null")) {
+                if (createDir("C://Games/src/main", "null")) {
+                    createDir("C://Games/src/main", "Main.java");
+                    createDir("C://Games/src/main", "Utils.java");
+                }
+                createDir("C://Games/src/test", "null");
+            }
+            ;
+            if (createDir("C://Games/res", "null")) {
+                createDir("C://Games/res/drawables", "null");
+                createDir("C://Games/res/vectors", "null");
+                createDir("C://Games/res/icons", "null");
+            }
+            createDir("C://Games/savegames", "null");
+            if (createDir("C://Games/temp", "null"))
+                createDir("C://Games/temp", "temp.txt");
+        }
+    }
+
+    public static boolean createDir(String path, String file) {
+
+        if (file.equals("null")) {
+
+            File objName = new File(path);
+
+            if (objName.mkdir()) {
+                System.out.println("Директория " + path + " создана");
+                return true;
+            } else {
+                System.out.println("Не удалось создать директорию " + path);
+                return false;
+            }
+        } else {
+
+            File objName = new File(path, file);
+
+            try {
+                if (objName.createNewFile())
+                    System.out.println("Файл " + file + " создан");
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+            return true;
+        }
     }
 }
